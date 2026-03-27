864bd0fdc3136cec058111c4ee6060d301ce0d1f
shubham-pawar01 <shubhamdev@mrchms.com>
2026-03-27 21:46:27 +0530

    code commented

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
-const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const client = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
 export async function POST(req) {
   try {
