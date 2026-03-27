cdd031e505f31d3c9f23248b3932c5e7da15e183
shubham-pawar01 <shubhamdev@mrchms.com>
2026-03-28 02:27:52 +0530

    database issue

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,15 +1,40 @@
+// import mongoose from "mongoose";
+
+// const MONGO_URL = process.env.MONGO_URL;
+
+// export const connectDB = async () => {
+//   try {
+//     if (mongoose.connection.readyState === 1) return;
+
+//     await mongoose.connect(MONGO_URL);
+
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.log("DB Error:", error);
+//   }
+// };
+
 import mongoose from "mongoose";
 
 const MONGO_URL = process.env.MONGO_URL;
 
-export const connectDB = async () => {
-  try {
-    if (mongoose.connection.readyState === 1) return;
+let isConnected = false;
 
-    await mongoose.connect(MONGO_URL);
+export const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    mongoose.set("bufferCommands", false); // ⭐ important
+
+    const db = await mongoose.connect(MONGO_URL, {
+      dbName: "portfolio", // ⭐ apna DB name daal (IMPORTANT)
+    });
+
+    isConnected = db.connections[0].readyState;
 
     console.log("MongoDB Connected");
   } catch (error) {
     console.log("DB Error:", error);
+    throw error; // ⭐ important (warna silent fail)
   }
 };
